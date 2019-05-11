c24d787b02cf5399eaaf6395e8eaeb1c49ecf247
TimurGorshkov <gorshkoff.timur@gmail.com>
2019-05-11 15:37:27 +0300

    Допилил рюказ и его методы

---
+++ src/recursive/Item.java
@@ -0,0 +1,26 @@
+package recursive;
+
+public class Item {
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    private String name;
+    private int weight;
+    private int price;
+
+    public Item(String name, int weight, int price) {
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+    }
+}
